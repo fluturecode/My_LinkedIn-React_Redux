f508144275bb71ce71f67b540e6d8218e817d7a1
Elizabeth Eidelson <fluturecode@gmail.com>
2021-01-13 11:39:16 -0500

    addded react flip move

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -9,8 +9,12 @@ import EventNoteIcon from "@material-ui/icons/EventNote"
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay"
 import { db } from "../firebase.js"
 import firebase from "firebase"
+import { useSelector } from "react-redux"
+import { selectUser } from "../features/userSlice"
+import FlipMove from 'react-flip-move'
 
 function Feed() {
+	const user = useSelector(selectUser)
 	const [posts, setPosts] = useState([])
 	const [input, setInput] = useState("")
 
@@ -31,10 +35,10 @@ function Feed() {
 		e.preventDefault()
 
 		db.collection("posts").add({
-			name: "Elizabeth Eidelson",
-			description: "This is a test",
+			name: user.displayName,
+			description: user.email,
 			message: input,
-			photoUrl: "",
+			photoUrl: user.photoUrl || "",
 			timestamp: firebase.firestore.FieldValue.serverTimestamp(),
 		})
 		setInput("")
@@ -67,6 +71,7 @@ function Feed() {
 					/>
 				</div>
 			</div>
+			<FlipMove>
 			{posts.map(({ id, data: { name, description, message, photoUrl } }) => (
 				<Post
 					key={id}
@@ -76,6 +81,7 @@ function Feed() {
 					photoUrl={photoUrl}
 				/>
 			))}
+			</FlipMove>
 		</div>
 	)
 }
